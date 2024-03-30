f8b0f9f5ce9771fe29fdf62188546b89f5cd9e42
0Aditya-Singhal0 <aditya.singhal1909@gmail.com>
2024-03-30 13:41:14 +0530

    Refactor chatbot model selection and response generation

--- app.py
+++ app.py
@@ -1,17 +1,7 @@
 import streamlit as st
-import torch
-from transformers import (
-    AutoConfig,
-    AutoModelForCausalLM,
-    AutoTokenizer,
-    BitsAndBytesConfig,
-    TextStreamer,
-)
-from accelerate import infer_auto_device_map, init_empty_weights
 import os
-import pathlib
-import textwrap
 import google.generativeai as genai
+from time import sleep as sleep
 
 
 # App title
@@ -19,15 +9,21 @@ st.set_page_config(page_title="🦙💬 Chatbot")
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-torch.cuda.empty_cache()
 
 with st.sidebar:
     st.title("🦙💬 Tagglabs Chatbot")
     selected_model = st.sidebar.selectbox(
         "Choose a model",
-        ["gemini-1.0-pro","gemini-1.0-pro-001","gemini-1.0-pro-latest","gemini-1.0-pro-vision-latest","gemini-pro","gemini-pro-vision",],
+        [
+            "gemini-pro",
+            "gemini-1.0-pro",
+            "gemini-1.0-pro-001",
+            "gemini-1.0-pro-latest",
+            "gemini-pro-vision",
+            "gemini-1.0-pro-vision-latest",
+        ],
         key="selected_model",
-        index = None,
+        index=0,
     )
 model = genai.GenerativeModel(selected_model)
 
@@ -53,6 +49,7 @@ def clear_chat_history():
         {"role": "assistant", "content": "How may I assist you today?"}
     ]
     st.session_state.history = []
+    
 
 
 st.sidebar.button("Clear Chat History", on_click=clear_chat_history)
@@ -62,48 +59,13 @@ def generate_response(prompt_input):
     # Add new conversation to history
     st.session_state.history.append({"role": "user", "content": f"{prompt_input}"})
 
-    messages = [
-        {
-            "role": "system",
-            "content": "You are a friendly anti-nsfw chatbot who always responds in short answer and crisp assistant voice. Only provide what is asked and nothing extra.",
-        },
-    ] + st.session_state.history
+    # Generate response
+    output = model.generate(prompt=prompt_input)
+    
+    # Append response to history
+    st.session_state.history.append({"role": "assistant", "content": output.text})
 
-    prompt_input = tokenizer.apply_chat_template(
-        messages, tokenize=False, add_generation_prompt=True, return_tensors="pt"
-    )
-
-    inputs = tokenizer(
-        prompt_input,
-        return_tensors="pt",
-    )
-
-    streamer = TextStreamer(tokenizer, skip_prompt=True, skip_special_tokens=True)
-
-    outputs = model.generate(
-        **inputs,
-        streamer=streamer,
-        use_cache=True,
-        max_new_tokens=10000,
-        temperature=0.1,
-        top_p=0.5,
-        repetition_penalty=1.15,
-        do_sample=True,
-        # early_stopping = True,
-        # num_beams = 1,
-        # top_k = 50,
-    )
-
-    output = tokenizer.decode(
-        outputs[0][inputs["input_ids"].shape[-1] :],
-        skip_special_tokens=True,
-        clean_up_tokenization_spaces=True,
-    )
-
-    # Append response from Llama2 to history
-    st.session_state.history.append({"role": "assistant", "content": output})
-
-    return output
+    return output.text
 
 
 # User-provided prompt
@@ -116,11 +78,12 @@ if prompt := st.chat_input():
 if st.session_state.messages[-1]["role"] != "assistant":
     with st.chat_message("assistant"):
         # with st.spinner("Thinking..."):
-        responses = generate_llama2_response(prompt)
+        responses = generate_response(prompt)
         full_response = ""
         message_placeholder = st.empty()
         for response in responses:
             full_response += response  # .choices[0].delta.get("content", "")
             message_placeholder.markdown(full_response + "▌")
+            sleep(0.1)
         message_placeholder.markdown(full_response)
     st.session_state.messages.append({"role": "assistant", "content": full_response})
